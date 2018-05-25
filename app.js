2775551e794c7b0743fe3314f62471703cc04585
Alex Choi <achoiweb@gmail.com>
2018-05-25 06:09:33 +0000

    practice react

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express'),
+      app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.render('index');
+})
+
+
+
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log(`[Server started]`);
+})
